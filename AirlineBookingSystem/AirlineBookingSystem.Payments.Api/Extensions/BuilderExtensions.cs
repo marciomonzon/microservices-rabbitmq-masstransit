af34d1d607bbcd0d73e66966ce0321c3c10ea4c7
monzondeveloper <monzondeveloper@gmail.com>
2025-01-24 20:50:13 -0300

    payment repo implemented

---
+++ AirlineBookingSystem/AirlineBookingSystem.Payments.Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using AirlineBookingSystem.Payments.Core.Repositories;
+using AirlineBookingSystem.Payments.Infrastructure.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AirlineBookingSystem.Payments.Api.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IDbConnection>(sp =>
+                    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void AddScopedServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+        }
+    }
+}
