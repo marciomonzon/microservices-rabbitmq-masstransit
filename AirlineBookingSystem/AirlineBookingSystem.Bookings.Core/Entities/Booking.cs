e0f7f3d4679c83b3e54c8624ca28b0a5b7d17468
monzondeveloper <monzondeveloper@gmail.com>
2025-01-24 18:40:35 -0300

    services entities

---
+++ AirlineBookingSystem/AirlineBookingSystem.Bookings.Core/Entities/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace AirlineBookingSystem.Bookings.Core.Entities
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        public Guid FlightId { get; set; }
+        public string PassengerName { get; set; }
+        public string SeatNumber { get; set; }
+        public DateTime BookingDate { get; set; }
+    }
+}
---
+++ AirlineBookingSystem/AirlineBookingSystem.Notifications.Core/Entities/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineBookingSystem.Notifications.Core.Entities
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public string Recipient { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; }
+    }
+}
