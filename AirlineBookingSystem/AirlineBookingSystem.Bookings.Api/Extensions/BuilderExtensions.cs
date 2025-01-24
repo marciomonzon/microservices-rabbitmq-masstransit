81584b4ac6a6b73bbce8b98daa32e433e3767a0d
monzondeveloper <monzondeveloper@gmail.com>
2025-01-24 19:21:19 -0300

    booking repository

---
+++ AirlineBookingSystem/AirlineBookingSystem.Bookings.Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AirlineBookingSystem.Bookings.Api.Extensions
+{
+    public static class BuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IDbConnection>(sp =>
+                    new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void AddMediatrCommands(this WebApplicationBuilder builder)
+        {
+            //builder.Services.AddMediatR(config =>
+            //config.RegisterServicesFromAssemblies(typeof(CreateUserCommand).Assembly));
+        }
+
+        public static void AddScopedServices(this WebApplicationBuilder builder)
+        {
+        }
+    }
+}
---
+++ AirlineBookingSystem/AirlineBookingSystem.Bookings.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,34 @@
+﻿using AirlineBookingSystem.Bookings.Core.Entities;
+using AirlineBookingSystem.Bookings.Core.Repositories;
+using Dapper;
+using System.Data;
+
+namespace AirlineBookingSystem.Bookings.Infrastructure.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public BookingRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task AddBookingAsync(Booking booking)
+        {
+            const string sql =
+                @"INSERT INTO Bookings(Id, FlightId, PassengerName, SeatName, BookingName) 
+                VALUES (@Id, @FlightId, @PassengerName, @SeatNumber, @BookingDate)";
+
+            await _dbConnection.ExecuteAsync(sql, booking);
+        }
+
+        public async Task<Booking> GetBookingByIdAsync(Guid Id)
+        {
+            const string sql = @"SELECT * FROM Bookins WHERE Id = @Id";
+
+            return await _dbConnection
+                         .QuerySingleOrDefaultAsync<Booking>(sql, new { Id = Id });
+        }
+    }
+}
