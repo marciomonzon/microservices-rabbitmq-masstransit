8507c0563719924f42aec5263e00573fd91b6ef5
monzondeveloper <monzondeveloper@gmail.com>
2025-01-31 20:56:22 -0300

    notification api implemented

---
+++ AirlineBookingSystem/AirlineBookingSystem.Notifications.Api/Controllers/NotificationController.cs
@@ -0,0 +1,25 @@
+﻿using AirlineBookingSystem.Notifications.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineBookingSystem.Notifications.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendNotification([FromBody] SendNotificationCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
