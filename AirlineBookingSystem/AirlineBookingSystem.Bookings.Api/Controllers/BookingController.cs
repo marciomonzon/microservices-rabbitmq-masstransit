31df77d3e3f6975596f3c8b5221a966786a8cb03
monzondeveloper <monzondeveloper@gmail.com>
2025-01-31 20:43:01 -0300

    booking api

---
+++ AirlineBookingSystem/AirlineBookingSystem.Bookings.Api/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using AirlineBookingSystem.Bookings.Application.Commands;
+using AirlineBookingSystem.Bookings.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineBookingSystem.Bookings.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BookingController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBooking([FromBody] CreateBookingCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetBookingById), new { id }, command);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBookingById(Guid id)
+        {
+            var booking = await _mediator.Send(new GetBookingQuery(id));
+            return Ok(booking);
+        }
+    }
+}
