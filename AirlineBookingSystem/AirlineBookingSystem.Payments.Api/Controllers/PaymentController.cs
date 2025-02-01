9be5ed6de62c937cf9cf83adb8365efff8498999
monzondeveloper <monzondeveloper@gmail.com>
2025-01-31 21:01:18 -0300

    payment api

---
+++ AirlineBookingSystem/AirlineBookingSystem.Payments.Api/Controllers/PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using AirlineBookingSystem.Payments.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirlineBookingSystem.Payments.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProcessPayment([FromBody] ProcessPaymentCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(ProcessPayment), new { id }, command);
+        }
+
+        [HttpDelete("refund/{id}")]
+        public async Task<IActionResult> RefundPayment([FromBody] RefundPaymentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+            
+        }
+    }
+}
