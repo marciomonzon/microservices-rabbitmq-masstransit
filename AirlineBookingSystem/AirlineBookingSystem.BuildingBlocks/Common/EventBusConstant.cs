562dec30e4ad741cf4b1cfa3596b6b13d7518942
monzondeveloper <monzondeveloper@gmail.com>
2025-02-04 18:58:38 -0300

    queue name fixed

--- AirlineBookingSystem/AirlineBookingSystem.BuildingBlocks/Common/EventBusConstant.cs
+++ AirlineBookingSystem/AirlineBookingSystem.BuildingBlocks/Common/EventBusConstant.cs
@@ -4,6 +4,6 @@
     {
         public const string FlightBookedQueue = "flight-booked-queue";
         public const string PaymentProcessedQueue = "payment-processed-queue";
-        public const string NotificationSentQueue = "notification=sent-queue";
+        public const string NotificationSentQueue = "notification-sent-queue";
     }
 }
